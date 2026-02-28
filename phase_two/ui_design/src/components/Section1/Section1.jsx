8477690442467cfc4b3a3808225caaea7924415b
Mr-Gopal <mr.gopal.programmer@gmail.com>
2026-02-28 10:35:28 +0530

    ui_design project intit

---
+++ phase_two/ui_design/src/components/Section1/Section1.jsx
@@ -0,0 +1,12 @@
+
+
+function Section1() {
+
+  return (
+    <div className="h-screen w-full bg-cyan-500">
+      
+    </div>
+  )
+}
+
+export default Section1
---
+++ phase_two/ui_design/src/components/Section2/Section2.jsx
@@ -0,0 +1,10 @@
+
+
+function Section2() {
+
+  return (
+    <div className="h-screen w-full bg-green-400">This is section 2</div>
+  )
+}
+
+export default Section2
