d779aadd34c5648795f78207d5c78b7165e3ede3
Mr-Gopal <mr.gopal.programmer@gmail.com>
2026-03-19 00:01:55 +0530

    two way binding

--- phase_three/formHandling/src/App.jsx
+++ phase_three/formHandling/src/App.jsx
@@ -3,10 +3,24 @@ import './App.css'
 
 function App() {
 
+  const [formData, setFormData] = useState("")
+
+  function submitHandler() {
+    console.log("Form submitted!", formData)
+    setFormData("")
+  }
+
+  
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder='Enter your name'/>
+      <form onSubmit={
+        (formObj) => {
+          formObj.preventDefault() // Prevents the autorefresh behaviour of a form while submitting
+          submitHandler()
+        }
+      }>
+        <input type="text" placeholder='Enter your name' value={formData} onChange={(e)=>{setFormData(e.target.value)}}/>
         <button>Submit</button>
       </form>
     </div>
