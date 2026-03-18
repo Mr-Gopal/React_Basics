fb5861b6da37acb41eb070b34779ed241fb7ed02
Mr-Gopal <mr.gopal.programmer@gmail.com>
2026-03-18 23:29:00 +0530

    init formHandling

---
+++ phase_three/formHandling/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder='Enter your name'/>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
