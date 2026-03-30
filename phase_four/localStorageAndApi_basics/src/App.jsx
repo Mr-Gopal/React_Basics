55735387a743d88a0bd9a0b63f2db04b2d984de2
Mr-Gopal <mr.gopal.programmer@gmail.com>
2026-03-30 23:44:03 +0530

    add comments

--- phase_four/localStorageAndApi_basics/src/App.jsx
+++ phase_four/localStorageAndApi_basics/src/App.jsx
@@ -4,15 +4,17 @@ import IdentityCard from './IdentityCard.jsx'
 import { useState } from 'react'
 
 function App() {
-  // const book = {
-  //   title: "The elegant universe",
-  //   author: "Brian Green",
-  //   pages: 500
-  // }
-  // localStorage.setItem("book", JSON.stringify(book))
-  // let bookData = JSON.parse(localStorage.getItem("book"))
-  // console.log(bookData);
+  // local storage
+  const book = {
+    title: "The elegant universe",
+    author: "Brian Green",
+    pages: 500
+  }
+  localStorage.setItem("book", JSON.stringify(book))
+  let bookData = JSON.parse(localStorage.getItem("book"))
+  console.log(bookData);
   
+  // Basic api handling
   const [data, setData] = useState({})
 
   const getData = async () => {
@@ -22,15 +24,7 @@ function App() {
     setData(info.data)
     console.log(data)
   }
-    // {
-    //   name: "gopal",
-    //   post: "engineer",
-    //   id: 43,
-    //   dob: 43,
-    //   phone: 3403402054,
-    //   email: "gopal@gmail.com",
-    //   profileImg: "https://hello.com"
-    // }
+
   return (
     <>
       <button onClick={getData}>Data</button>
