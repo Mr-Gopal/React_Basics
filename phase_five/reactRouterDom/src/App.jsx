33e875e4be78771e92bb58968dfc39e6befebb5a
Mr-Gopal <mr.gopal.programmer@gmail.com>
2026-04-04 17:43:00 +0530

    basic routing complete

--- phase_five/reactRouterDom/src/App.jsx
+++ phase_five/reactRouterDom/src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import About from './Pages/About.jsx'
+import Contact from './Pages/Contact.jsx'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
 
   return (
-    <h1>hello</h1>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
+    </div>
   )
 }
 
